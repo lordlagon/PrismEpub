40241d51d7a2c59e3aadd80d724fe142e7dea314
Andre Xavier <andre.macedo@aluno.positivo.edu.br>
2019-01-16 13:53:49 -0200

    mudança para carrousel page

--- PrismEpub/PrismEpub/Views/MainPage.xaml.cs
+++ PrismEpub/PrismEpub/Views/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace PrismEpub.Views
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : CarouselPage
     {
         public MainPage()
         {
